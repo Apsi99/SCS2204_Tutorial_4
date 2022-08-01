f59bcc1b89391321ac683c9c8c95f94babc2aa81
Apsara <91180869+Apsi99@users.noreply.github.com>
2022-08-01 20:58:30 +0530

    Add files via upload
    scala files are located here:   src/main/scala

---
+++ 20000693/src/main/scala/20000693 Question2.scala
@@ -0,0 +1,21 @@
+import scala.io.StdIn.readInt
+
+object Question2 {
+  def pattern(n:Int):Unit= n match {
+    case y if y<0 =>println("Negative")
+    case y if y==0 =>println("Zero")
+    case y if y%2==0 =>println("Even")
+    case y if y%2==1 =>println("Odd")
+  }
+
+  def main(args: Array[String]): Unit = {
+    println("Enter your integer input: ")
+    var num= readInt()
+    pattern(num)
+  }
+
+
+
+
+
+}
